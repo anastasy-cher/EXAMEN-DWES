b6745841b31d4cf16ebcdc20df36eeb6a7467994
anstcher <anastasiacher73@gmail.com>
2023-02-22 10:50:37 +0100

    add funccional

--- database/db.sql
+++ database/db.sql
@@ -1,10 +1,12 @@
+drop database if exists fotos;
 create database fotos;
 use fotos;
 
 create table fotos(
     id int auto_increment primary key not null,
-    descripcion varchar(250) not null,
+    url text not null,
     titulo varchar(250) not null,
+    descripcion varchar(250) not null,
     fecha timestamp not null default current_timestamp,
     likes int,
     dislikes int
--- routes/fotos.js
+++ routes/fotos.js
@@ -1,10 +1,31 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const pool = require('../database')
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('/fotos');
+  
+  res.send('galeria')
 });
+
+router.get('/add', function(req, res, next) {
+  res.render('fotos/add')
+});
+router.post('/add', async (req, res, next) => {
+  const {url, titulo, descripcion} = req.body
+  const newPub = {
+    url,
+    titulo,
+    descripcion
+  }
+
+  pool.query('INSERT INTO fotos SET ?', [newPub])
+  console.log(req.body)
+  res.send('add')
+});
+
 
 router.get('/masvotadas', function(req, res, next) {
   res.send('/masvotadas');
